9859b13fa97267682b6286782aae32e07a52f0d2
ayamte <ayamtejjal123@gmail.com>
2024-08-06 14:15:12 +0100

    layout is added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 import {Button} from 'react-bootstrap'
 import {Entry} from "./page/entry/Entry.page";
+import DefaultLayout from "./layout/DefaultLayout";
 
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/*<Entry />*/}
+      <DefaultLayout>
+        //Dashboard
+      </DefaultLayout>
     </div>
   );
 }
