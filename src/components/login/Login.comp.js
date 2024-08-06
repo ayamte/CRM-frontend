0a58cfa51b109165200af959d2e7489d3b374ea8
ayamte <ayamtejjal123@gmail.com>
2024-08-06 01:15:44 +0100

    App set up. added loging and reset password

---
+++ src/components/login/Login.comp.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import  {Container, Row, Col, Form, Button} from 'react-bootstrap'
+import { propTypes } from 'react-bootstrap/esm/Image';
+
+export const LoginForm = ({handleOnChange, handleOnSubmit, frmSwitcher, email, pass}) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+        <h1 className="text-info text-center">Client Login</h1>
+        <hr/>
+        <Form autoComplete="off" onSubmit={handleOnSubmit}>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+             type="email"
+             name="email"
+             value={email}
+             onChange = {handleOnChange}
+             placeholder="Enter Email"
+             required
+             
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+             type="password"
+             name="password"
+             value={pass}
+             onChange = {handleOnChange}
+             placeholder="password"
+             required
+             
+            />
+          </Form.Group>
+
+
+          <Button type="submit">Login</Button>
+        </Form>
+        <hr/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href="#!" onClick ={()=> frmSwitcher('reset')}>Forget Password?</a>
+        </Col>
+
+      </Row>
+
+    </Container>
+  )
+}
+
+LoginForm.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  handleOnSubmit: PropTypes.func.isRequired,
+  frmSwitcher: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  pass: PropTypes.string.isRequired,
+}
---
+++ src/page/entry/Entry.page.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+
+import {Container} from 'react-bootstrap';
+import {ResetPassword} from '../../components/password-reset/PasswordReset.comp';
+import {LoginForm} from '../../components/login/Login.comp';
+import './entry.style.css';
+
+
+
+
+export const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [frmLoad, setFrmLoad] = useState("login");
+
+  const handleOnChange = (e) =>{
+    const {name, value} = e.target;
+
+    switch(name){
+      case 'email':
+        setEmail(value);
+        break;
+        
+        case 'password':
+          setPassword(value);
+          break;
+
+        default:
+          break;
+    }
+  };
+
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+
+    if(!email || !password){
+      return alert("Fill up all the form!");
+    }
+  };
+
+  const handleOnResetSubmit = (e) =>{
+    e.preventDefault();
+
+    if(!email){
+      return alert("Please enter the email!");
+    }
+  };
+
+  const frmSwitcher = (frmType) =>{
+    setFrmLoad(frmType);
+  }
+
+  return <div className="entry-page bg-info">
+    <Container className="p-5 mb-4 bg-light rounded-3 form-box">
+      <div className="container-fluid py-5">
+        {frmLoad ==='login' && <LoginForm handleOnChange={handleOnChange} 
+        handleOnSubmit={handleOnSubmit}
+        frmSwitcher = {frmSwitcher}
+        email={email}
+        pass={password}
+      
+        />}
+        
+
+        {frmLoad === 'reset' && <ResetPassword handleOnChange={handleOnChange} 
+        handleOnResetSubmit={handleOnResetSubmit}
+        frmSwitcher = {frmSwitcher}
+        email={email}
+        pass={password}
+                
+        />}
+        
+      </div>
+    </Container>
+  </div>;
+};
